d12a419d86495b9eee6a4a66140f6185ee01ee8f
ajaykanth19 <ajaykanth1921@gmail.com>
2020-08-19 14:50:13 +0530

    modified first.java

--- first.java
+++ first.java
@@ -1,18 +1,9 @@
- Volume in drive D has no label.
- Volume Serial Number is BC38-F272
+public class first{
+	public static void main(String[] args{
 
- Directory of D:\Devops latest\My project
+	for(int i=1;i<=10;i++){
 
-08/19/2020  02:42 PM    <DIR>          .
-08/19/2020  02:42 PM    <DIR>          ..
-08/19/2020  02:42 PM                 0 first.java
-08/18/2020  09:58 PM               354 test1.txt
-08/18/2020  10:02 PM               402 test2.txt
-08/18/2020  10:03 PM               453 test3.html
-08/18/2020  10:06 PM                44 test3.txt
-08/18/2020  10:08 PM                 0 test4.txt
-08/18/2020  11:02 PM               604 test5.html
-08/18/2020  11:47 PM               654 test5.txt
-08/19/2020  12:03 AM               704 test6.txt
-               9 File(s)          3,215 bytes
-               2 Dir(s)  414,572,834,816 bytes free
+	System.out.println("Hello World..."+i);
+}
+}
+}
